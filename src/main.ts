2b21707626d769e5ef89d3ac27fa9b6bdb6416b7
esaramago <dev@emanuelsaramago.com>
2025-03-20 22:35:57 +0000

    refactor main.ts to use environment variables for configuration

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,9 @@
 import fetchData from './js/fetchData'
 import generateLabel from './js/generateLabel'
 
-const sheetId = '1Nq33YMBrCi4XvupSgUI4KSuPA_vOUNC8t1-l3_0BciM'
-const range = 'TESTE!A2:M1000' // Replace with the desired range
-const apiKey = 'AIzaSyDrCTjhpYJz1YsTry1YniUWK9b2swr4Ibk' // Replace with your Google Sheets API key
+const sheetId = import.meta.env.VITE_SHEET_ID as string
+const range = import.meta.env.VITE_RANGE as string
+const apiKey = import.meta.env.VITE_API_KEY as string
 
 let labelsHTML = ''
 const products = await fetchData(sheetId, range, apiKey)
