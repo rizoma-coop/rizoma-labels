f84ba4ff00db3e77f7c40f6ef20500231f69f144
esaramago <dev@emanuelsaramago.com>
2025-10-02 12:18:39 +0100

    switch design ongoing

--- src/js/generateLabel.ts
+++ src/js/generateLabel.ts
@@ -1,6 +1,6 @@
 export default function generateLabel(product: Array<string>) {
 
-  const [name, unit, brand, origin, pvp, iva, markup, price] = product
+  const [name, unit, brand, iva, markup, score] = product
 
   function getUnit(unit: string) {
     unit = unit.toLowerCase()
@@ -15,43 +15,62 @@ export default function generateLabel(product: Array<string>) {
 
   }
 
-  return `
+  return /* html */`
 
     <div class="card">
       <div class="card__header">
         <h2 class="card__name">${name}</h2>
-        <div class="u-uppercase">Marca, origem</div>
-        <h3 class="card__brand">
-          ${brand}
-          ${
-            origin ? `, ${origin}` : ''
-          }
-        </h3>
+        <h3 class="card__brand">${brand}</h3>
       </div>
       <div class="card__body">
         <div class="card__price">
-          <span class="card__pvp">${pvp}</span>
           <span class="card__unit">€${getUnit(unit)}</span>
         </div>
         <div class="card__details">
           <table>
+            <tr>
+              <td>P.Custo</td>
+              <td class="u-font-bold u-text-right">€</td>
+            </tr>
             <tr>
               <td>IVA</td>
               <td class="u-font-bold u-text-right">${iva || ''}%</td>
             </tr>
             <tr>
               <td>Mark-up</td>
-              <td class="u-font-bold u-text-right">${markup || ''}%</td>
-            </tr>
-            <tr>
-              <td>P.Custo</td>
-              <td class="u-font-bold u-text-right">${price || ''}€</td>
+              <td class="u-font-bold u-text-right">${markup.split(',')[1] || ''}%</td>
             </tr>
           </table>
         </div>
       </div>
       <div class="card__footer">
-          <img src="footer.webp" alt="footer" class="card__footer-img" />
+        <div class="score score--large"><!--${score}-->A</div>
+        <ul>
+          <li>
+            <span class="score">B</span>
+            Lorem ipsum
+          </li>
+          <li>
+            <span class="score">B</span>
+            Lorem ipsum
+          </li>
+          <li>
+            <span class="score">C</span>
+            Lorem ipsum
+          </li>
+          <li>
+            <span class="score">B</span>
+            Lorem ipsum
+          </li>
+          <li>
+            <span class="score">B</span>
+            Lorem ipsum
+          </li>
+          <li>
+            <span class="score">C</span>
+            Lorem ipsum
+          </li>
+        </ul>
       </div>
     </div>
   `
